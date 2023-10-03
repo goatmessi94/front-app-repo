6968d41ee38e2a0123eb83615865874fa59a40c9
asj2 <asj@bizspring.co.kr>
2023-10-03 18:52:42 +0900

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog Version 1
+            EKS DEMO Blog Version 안성재
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
